d5a47143553d9d6003e893c2a5b058241768c241
Ren <vergaraianreniel@gmail.com>
2025-03-03 16:57:13 +0800

    second commit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,23 @@
 const Sidebar = () => {
     return ( 
         <div className="sidebar">
-            
+        <ul className="list-unstyled"> 
+            <li>
+                <i class="bi bi-house-door"></i> Dashboard  
+            </li>
+            <li>
+                <i class="bi bi-people-fill"></i> Users  
+            </li>
+            <li>
+                <i class="bi bi-box-seam"></i> Products 
+            </li>
+            <li>
+                <i class="bi bi-coin"></i> Sales 
+            </li>
+            <li>
+                <i class="bi bi-bar-chart-line"></i> Report  
+            </li>
+            </ul>
         </div>
      );
 }
