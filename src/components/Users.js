9700d16ada34886a1e90905e60f707d71ff6587e
Ren <vergaraianreniel@gmail.com>
2025-03-05 13:23:35 +0800

    7th commit

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,31 +1,31 @@
-import React, { useEffect, useRef } from 'react';
-import $ from 'jquery'; // Import jQuery (required by DataTables)
-import 'datatables.net-dt';
-import 'datatables.net-select-dt';
-import 'datatables.net-responsive-dt';
-import 'datatables.net-dt/css/dataTables.dataTables.min.css';
-import './styling/Users.css';
+import React, { useEffect, useRef } from "react";
+import $ from "jquery"; // Import jQuery (required by DataTables)
+import "datatables.net-dt";
+import "datatables.net-select-dt";
+import "datatables.net-responsive-dt";
+import "datatables.net-dt/css/dataTables.dataTables.min.css";
+import "./styling/Users.css";
 
 // Import the necessary button extensions and their CSS
-import 'datatables.net-buttons-dt';
-import 'datatables.net-buttons/js/buttons.colVis.min';
-import 'datatables.net-buttons/js/buttons.html5.min';
-import 'datatables.net-buttons/js/buttons.print.min';
-import 'datatables.net-buttons-dt/css/buttons.dataTables.min.css'; // Button CSS File
+import "datatables.net-buttons-dt";
+import "datatables.net-buttons/js/buttons.colVis.min";
+import "datatables.net-buttons/js/buttons.html5.min";
+import "datatables.net-buttons/js/buttons.print.min";
+import "datatables.net-buttons-dt/css/buttons.dataTables.min.css"; // Button CSS File
 
 const Users = () => {
   const tableRef = useRef(null);
   const buttonContainerRef = useRef(null);
 
   const columns = [
-    { title: 'Name', data: 'name' },
-    { title: 'Username', data: 'username' },
-    { title: 'Lastlogin', data: 'email' },
+    { title: "Name", data: "name" },
+    { title: "Username", data: "username" },
+    { title: "Lastlogin", data: "email" },
   ];
 
   const data = [
-    { name: 'Dean', username: 'D', email: 'dean@westfields.edu.ph' },
-    { name: 'Robert', username: 'R', email: 'Robert@westfields.edu.ph' },
+    { name: "Dean", username: "D", email: "dean@westfields.edu.ph" },
+    { name: "Robert", username: "R", email: "Robert@westfields.edu.ph" },
   ];
 
   useEffect(() => {
@@ -34,9 +34,7 @@ const Users = () => {
       const dt = $(tableRef.current).DataTable({
         responsive: true,
         select: true,
-        buttons: [
-          'copy', 'csv', 'excel', 'print', 'colvis'
-        ],
+        buttons: ["copy", "csv", "excel", "print", "colvis"],
       });
 
       // Move the buttons to the custom container
@@ -51,13 +49,10 @@ const Users = () => {
 
   return (
     <div className="usersbody">
-      
-
       {/* DataTable */}
       <h1>Users</h1>
       <table ref={tableRef} className="display cell-border">
         <thead>
-
           <tr>
             {columns.map((col, index) => (
               <th key={index}>{col.title}</th>
